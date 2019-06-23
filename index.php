f26751a91af4c970536ad2a0b7ae8099c62e1341
frank collins <frank@localhost.localdomain>
2019-06-23 10:41:17 -0400

    updates

--- index.php
+++ index.php
@@ -19,34 +19,6 @@
   <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
-<!--outsite-->
-<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
-      <link href="https://fonts.googleapis.com/css?family=Raleway:400,600,700" rel="stylesheet">
-      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
-
-
-      <link rel="stylesheet" href="assets/css/slick.css">
-      <link rel="stylesheet" href="assets/css/slick-theme.css">
-      <link rel="stylesheet" href="assets/css/animate.css">
-      <link rel="stylesheet" href="assets/css/fonticons.css">
-      <link rel="stylesheet" href="assets/css/font-awesome.min.css">
-      <link rel="stylesheet" href="assets/css/bootstrap.css">
-      <link rel="stylesheet" href="assets/css/magnific-popup.css">
-      <link rel="stylesheet" href="assets/css/bootsnav.css">
-
-
-      <!--For Plugins external css-->
-      <!--<link rel="stylesheet" href="assets/css/plugins.css" />-->
-
-      <!--Theme custom css -->
-      <link rel="stylesheet" href="assets/css/style.css">
-      <!--<link rel="stylesheet" href="assets/css/colors/maron.css">-->
-
-      <!--Theme Responsive css-->
-      <link rel="stylesheet" href="assets/css/responsive.css" />
-
-      <script src="assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
-<!--endout-->
   <!-- Custom styles for this template -->
   <!--<link href="css/agency.min.css" rel="stylesheet">-->
   <link rel="stylesheet" href="css/agency.css">
